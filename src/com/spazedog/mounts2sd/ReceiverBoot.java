44164a6d32cad54e3072f238d9ea27aa60988a73
Daniel Bergløv <d.bergloev@gmail.com>
2013-08-28 13:54:28 +0200

    Add Boot Receiver Each time the application is launched after a boot, it takes longer to load as it needs to check all of the device setups. So we will do this automatically after each boot to speed up the first launch for users

---
+++ src/com/spazedog/mounts2sd/ReceiverBoot.java
@@ -0,0 +1,71 @@
+package com.spazedog.mounts2sd;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.spazedog.mounts2sd.tools.Preferences;
+import com.spazedog.mounts2sd.tools.Root;
+
+public class ReceiverBoot extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+			new Thread() {
+				private Context mContext;
+				
+				public Thread putContext(Context context) {
+					mContext = context;
+					
+					return this;
+				}
+				
+				@Override
+				public void run() {
+					String message = null;
+					
+					if (Root.isConnected()) {
+						Root.lock("ReceiverBoot");
+						Preferences preferences = new Preferences(mContext);
+						
+						if (!preferences.loadAll(false)) {
+							message = mContext.getResources().getString(R.string.notify_no_config);
+							
+						} else if (preferences.deviceSetup().log_level() > 1) {
+							message = mContext.getResources().getString(R.string.notify_log_details);
+						}
+						
+						Root.unlock("ReceiverBoot");
+						Root.close();
+						
+					} else {
+						message = mContext.getResources().getString(R.string.notify_no_config);
+					}
+					
+					if (message != null) {
+						NotificationManager manager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+						
+						NotificationCompat.Builder notification = new NotificationCompat.Builder(mContext).setContentIntent(PendingIntent.getActivity(
+								mContext,
+								0,
+								new Intent(mContext, ActivityTabController.class),
+								0
+						))
+						.setSmallIcon(R.drawable.ic_launcher)
+						.setWhen(System.currentTimeMillis())
+						.setAutoCancel(true)
+						.setContentTitle( mContext.getApplicationInfo().name )
+						.setContentText(message);
+						
+						manager.notify(1, notification.build());
+					}
+				}
+				
+			}.putContext(context.getApplicationContext()).start();
+		}
+	}
+}
